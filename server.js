cbe8d094d50d608c843dd79a7a023f2f957a9e21
Matthew Gittens <msgittens@mail.fhsu.edu>
2023-05-09 16:50:35 -0400

    Commit

--- server.js
+++ server.js
@@ -15,6 +15,14 @@ connectDB(db);
 //Middleware
 app.use(cors());
 
+app.use((req, res, next) =>{
+    const allowedOrigins = ['*'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+})
+
 app.options("*", (req, res, next)=>{
     res.header('Access-Control-Allow-Origin', "*")
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS');
