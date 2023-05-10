2a76e346c4ec276f6826e31d66eaead9637c9333
Matthew Gittens <msgittens@mail.fhsu.edu>
2023-05-10 10:48:52 -0400

    Whole lot in this one

--- Controllers/statesController.js
+++ Controllers/statesController.js
@@ -24,15 +24,82 @@ const getStateCapital = async (req, res) =>{
     try{
        const statecode = data.states;
        const ste = await data2.filter(s => s.code === req.params.state );
-       if(!ste) return res.status(404).json({message: 'Invalid state abbreviation parameter'});
+       console.log(ste);
        result = ste
         .map(({ state }) => state);
        result2 = ste  
         .map(({capital_city}) => capital_city);
        // res.json(result);
       var JSONObj = {"state": `${result}`,
-                     "Capital City": `${result2}`   
+                     "capital": `${result2}`   
 };
+if(JSONObj.state == "") return res.status(404).json({message: 'Invalid state abbreviation parameter'});
+res.json(JSONObj)
+   }catch(err){
+       res.status(500).send(err.message);
+   }
+   
+   };
+   
+//Get specific state and nickname
+const getStateNickname = async (req, res) =>{
+    try{
+       const statecode = data.states;
+       const ste = await data2.filter(s => s.code === req.params.state );
+       console.log(ste);
+       result = ste
+        .map(({ state }) => state);
+       result2 = ste  
+        .map(({nickname}) => nickname);
+       // res.json(result);
+      var JSONObj = {"state": `${result}`,
+                     "nickname": `${result2}`   
+};
+if(JSONObj.state == "") return res.status(404).json({message: 'Invalid state abbreviation parameter'});
+res.json(JSONObj)
+   }catch(err){
+       res.status(500).send(err.message);
+   }
+   
+   };
+   
+//Get specific state and population
+const getStatePopulation = async (req, res) =>{
+    try{
+       const statecode = data.states;
+       const ste = await data2.filter(s => s.code === req.params.state );
+       console.log(ste);
+       result = ste
+        .map(({ state }) => state);
+       result2 = ste  
+        .map(({population}) => population);
+       // res.json(result);
+      var JSONObj = {"state": `${result}`,
+                     "population": `${result2}`   
+};
+if(JSONObj.state == "") return res.status(404).json({message: 'Invalid state abbreviation parameter'});
+res.json(JSONObj)
+   }catch(err){
+       res.status(500).send(err.message);
+   }
+   
+   };
+   
+//Get specific state and admission
+const getStateAdmission  = async (req, res) =>{
+    try{
+       const statecode = data.states;
+       const ste = await data2.filter(s => s.code === req.params.state );
+       console.log(ste);
+       result = ste
+        .map(({ state }) => state);
+       result2 = ste  
+        .map(({admission_date}) => admission_date);
+       // res.json(result);
+      var JSONObj = {"state": `${result}`,
+                     "admitted": `${result2}`   
+};
+if(JSONObj.state == "") return res.status(404).json({message: 'Invalid state abbreviation parameter'});
 res.json(JSONObj)
    }catch(err){
        res.status(500).send(err.message);
@@ -44,8 +111,11 @@ res.json(JSONObj)
 //Exports
 module.exports = {
     getAllStates,
-    getStateCapital
-   // checkIfContig,
+    getStateCapital,
+    getStateNickname,
+    getStatePopulation,
+    getStateAdmission
+   
    // getAllContigStates,
 
 }
