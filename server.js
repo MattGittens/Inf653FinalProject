0b043fd70e90799da8b2c391fbce3fce49e11b23
Matthew Gittens <msgittens@mail.fhsu.edu>
2023-05-09 22:13:58 -0400

    whole lotta commitin

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const connectDB = require('./config/db');
 const path = require ('path');
 const fs = require('fs');
 const corsOptions = require('./config/corsOptions');
+const errorHandler = require('./middleware/errorHandler.js');
+const { logger } = require('./middleware/logEvents');
+
+
+
+
 
 const app = express();
 const db = process.env.DATABASE_URI;
@@ -14,11 +20,12 @@ const PORT = process.env.PORT || 3000;
 connectDB(db);
 
 //Middleware
-
+app.use(logger);
 
 app.use(cors(corsOptions));
 app.use(express.json());
 
+//Static Files
 app.use('/', express.static(path.join(__dirname, '/public')));
 //
 app.use((req, res, next)=>{
@@ -31,5 +38,19 @@ app.use((req, res, next)=>{
 app.use('/', require('./routes/api/states'));
 app.use('/', require('./routes/api/stateCode'));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
+});
+app.use(errorHandler);
 
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
