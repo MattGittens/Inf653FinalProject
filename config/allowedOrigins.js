5538a9c7a0d09b4cc0247a7999dc142545baff3a
Matthew Gittens <msgittens@mail.fhsu.edu>
2023-05-09 21:37:59 -0400

    l

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,6 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
-    'https://dazzling-snickerdoodle-777101.netlify.app/'
+    'https://dazzling-snickerdoodle-777101.netlify.app'
 ];
 module.exports = allowedOrigins;
